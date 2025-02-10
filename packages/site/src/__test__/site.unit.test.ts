a204bc2314b0be9b86fffd5fe5a37a12013da5f3
jimAScott <jim_a_scott@hotmail.com>
2025-02-10 15:53:18 +0000

    test: update the test files

--- packages/site/src/__test__/site.unit.test.ts
+++ packages/site/src/__test__/site.unit.test.ts
@@ -4,8 +4,8 @@ import Welcome from '../components/Welcome.astro';
 
 describe('Welcome component test', () => {
     it('should render the welcome message', async () => {
-        const container = await AstroContainer.create();
-        const welcome = await container.renderToString(Welcome);
+        const container = await AstroContainer.create() as AstroContainer;
+        const welcome = await container.renderToString(Welcome) as string;
         expect(welcome).toContain('To get started');
     });
 });
