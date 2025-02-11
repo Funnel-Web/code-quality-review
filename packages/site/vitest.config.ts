da4bb2ce4f6e22b826c65073746eac0db2534c6e
jimAScott <jim_a_scott@hotmail.com>
2025-02-11 17:43:06 +0000

    fix: add repoting path to sonar

--- packages/site/vitest.config.ts
+++ packages/site/vitest.config.ts
@@ -3,6 +3,8 @@ import { getViteConfig } from 'astro/config';
 
 export default getViteConfig({
     test: {
-        // Vitest configuration options
+        reporters: [
+            ['default'],
+        ],
     },
 });
