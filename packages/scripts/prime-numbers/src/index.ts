d0642087f0c800dcfab1442d0ee1c17fd0df18a0
James Scott <jim_a_scott@hotmail.com>
2025-02-11 14:37:40 +0000

    feat: add another function with tests (#5)
    * feat: add another function with tests
    
    * fix: update cor
    verage report for sonar

--- packages/scripts/prime-numbers/src/index.ts
+++ packages/scripts/prime-numbers/src/index.ts
@@ -1,5 +1,7 @@
-import { getPrimes } from './prime';
+import { getPrimes, getNextPrime } from './prime';
 
 const limit = 1000;
 const result = getPrimes(limit);
 console.log(`primes from 1 to ${limit} are: ${result.join(',')}`);
+
+console.log('The prime after 13 is', getNextPrime(13));
